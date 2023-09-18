90d88f91fef0cae6a8bd1ded1a3e77fb8b72ef43
DaniRoses <115042715+DaniRoses@users.noreply.github.com>
2023-09-19 01:04:52 +0330

    Added connection to database and  get connection method

--- src/main/java/connection/JdbcConnection.java
+++ src/main/java/connection/JdbcConnection.java
@@ -1,4 +1,16 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcConnection {
+    private final Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "amir9650");
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public JdbcConnection() throws SQLException {
+    }
 }

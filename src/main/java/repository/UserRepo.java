5a6b32c32eec30be304825c6623671ae41ef5935
DaniRoses <115042715+DaniRoses@users.noreply.github.com>
2023-09-19 12:57:46 +0330

    Added a save method in UserRepo

--- src/main/java/repository/UserRepo.java
+++ src/main/java/repository/UserRepo.java
@@ -1,4 +1,34 @@
 package repository;
 
+import connection.JdbcConnection;
+import model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class UserRepo {
+    JdbcConnection jdbcConnection = new JdbcConnection();
+    Connection connection = jdbcConnection.getConnection();
+
+    public UserRepo() throws SQLException {
+
+    }
+
+    public void save(User user) throws SQLException {
+        String query = "INSERT INTO users(name, username, email, password) values (?,?,?,?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getUsername());
+        preparedStatement.setString(3, user.getEmail());
+        preparedStatement.setString(4, user.getPassword());
+        int result = preparedStatement.executeUpdate();
+
+        if (result > 0) {
+            System.out.println("+Successfully registered");
+        } else {
+            System.out.println("-Failed to register");
+        }
+    }
 }

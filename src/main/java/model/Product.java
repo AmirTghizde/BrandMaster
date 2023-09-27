bc82f3a71640bd7966a8a462c29011553f9773e2
DaniRoses <115042715+DaniRoses@users.noreply.github.com>
2023-09-27 17:59:49 +0330

    Added category and brand as an attribute and changed createDate type to string

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -5,16 +5,16 @@ import javax.xml.crypto.Data;
 public class Product {
     private Integer id;
     private String name;
-    private Data createDate;
+    private String createDate;
     private Category category;
     private Brand brand;
 
-    public Product(Integer id, String name, Data createDate) {
+    public Product(Integer id, String name, String createDate) {
         this(name, createDate);
         this.id = id;
     }
 
-    public Product(String name, Data createDate) {
+    public Product(String name, String createDate) {
         this.name = name;
         this.createDate = createDate;
     }
@@ -38,11 +38,27 @@ public class Product {
         this.name = name;
     }
 
-    public Data getCreateDate() {
+    public String getCreateDate() {
         return createDate;
     }
 
-    public void setCreateDate(Data createDate) {
+    public void setCreateDate(String createDate) {
         this.createDate = createDate;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
     }
 }

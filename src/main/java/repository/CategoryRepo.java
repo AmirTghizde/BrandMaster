714e3e078c69c8a2248dacb15dec94ee1a6010bb
DaniRoses <115042715+DaniRoses@users.noreply.github.com>
2023-09-27 18:59:29 +0330

    Added show method

--- src/main/java/repository/CategoryRepo.java
+++ src/main/java/repository/CategoryRepo.java
@@ -65,6 +65,19 @@ public class CategoryRepo {
         return resultSet.next();
     }
 
+    public void showCategory() throws SQLException {
+        String query = "select * from category";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()) {
+            int id = (resultSet.getInt("id"));
+            String name = (resultSet.getString("name"));
+            String description = (resultSet.getString("description"));
+            System.out.println("\t"+id+".\t\t"+name+"\t\t"+description );
+        }
+    }
+
 public void printResult(int result){
         if (result>0){
             System.out.println("+Successful");

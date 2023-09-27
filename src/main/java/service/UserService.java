207ac949842d69aab2a9385a3cb180dfad5482f2
DaniRoses <115042715+DaniRoses@users.noreply.github.com>
2023-09-27 21:26:43 +0330

    added UserService methods

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -4,16 +4,17 @@ import model.User;
 import repository.UserRepo;
 
 import java.sql.SQLException;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class UserService {
     UserRepo userRepo = new UserRepo();
+    Scanner sc = new Scanner(System.in);
 
     public UserService() throws SQLException {
     }
 
     public void register() throws SQLException {
-        Scanner sc = new Scanner(System.in);
         boolean repeat = true;
         while (repeat) {
             System.out.print("Please enter your name: ");
@@ -43,15 +44,72 @@ public class UserService {
     }
 
     public void login() throws SQLException {
-        Scanner sc = new Scanner(System.in);
         System.out.print("Enter your username: ");
         String username = sc.nextLine();
         System.out.print("Enter your password: ");
         String password = sc.nextLine();
 
         User user = userRepo.login(username);
-        if (user!=null&& user.getPassword().equals(password)){//if the user is not null and the password is the same...
+        if (user != null && user.getPassword().equals(password)) {//if the user is not null and the password is the same...
             System.out.println("Successfully logged in :D");
-        }else System.out.println("Wrong username/password");
+        } else System.out.println("Wrong username or password");
+    }
+
+    public void changeName() throws SQLException {
+        //TODO change this part and make it dynamic so the user only sends the new name
+        System.out.print("Enter your id: ");//temporary
+        int id = sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter the new name: ");
+        String name = sc.nextLine();
+
+        userRepo.editName(id, name);
+    }
+
+    public void changeUsername() throws SQLException {
+        System.out.print("Enter your current username: ");
+        String username = sc.nextLine();
+        System.out.print("Enter the new username: ");
+        String newUsername = sc.nextLine();
+
+        if (userRepo.isUsernameExists(newUsername)) {
+            System.out.println("!Username is taken");
+        } else userRepo.editUsername(username, newUsername);
+    }
+
+    public void changeEmail() throws SQLException {
+        System.out.print("Enter your current email: ");
+        String email = sc.nextLine();
+        System.out.print("Enter the new email: ");
+        String newEmail = sc.nextLine();
+
+        if (userRepo.isUsernameExists(newEmail)) {
+            System.out.println("!Email is already being used");
+        } else userRepo.editEmail(email, newEmail);
+    }
+
+    public void changePassword() throws SQLException {
+        //TODO change this part and make it dynamic so the user only sends the new password
+        boolean repeat = true;
+        System.out.print("Enter your id: ");//temporary
+        int id = sc.nextInt();
+        sc.nextLine();
+        while (repeat) {
+            System.out.print("Enter the new password: ");
+            String password = sc.nextLine();
+            System.out.print("Enter the new password again: ");
+            String passwordRepeat = sc.nextLine();
+            if (Objects.equals(password, passwordRepeat)) {
+                repeat=false;
+                userRepo.editPassword(id, password);
+            } else System.out.println("!Repeated password doesn't match");
+        }
+    }
+    public void deleteAccount() throws SQLException {
+        //TODO change this part and make it dynamic so the user only hits delete
+        System.out.print("Enter your id: ");//temporary
+        int id = sc.nextInt();
+        sc.nextLine();
+        userRepo.delete(id);
     }
 }

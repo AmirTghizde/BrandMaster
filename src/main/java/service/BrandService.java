eddeec57f17671a36271af5bed4452f7f6219c2d
DaniRoses <115042715+DaniRoses@users.noreply.github.com>
2023-09-27 23:31:47 +0330

    Tweaked some methods

--- src/main/java/service/BrandService.java
+++ src/main/java/service/BrandService.java
@@ -14,48 +14,59 @@ public class BrandService {
     }
 
     public void add() throws SQLException {
-        System.out.print("Enter brand name: ");
-        String name = sc.nextLine();
-        System.out.print("Enter brand website: ");
-        String website = sc.nextLine();
-        System.out.print("Enter brand description: ");
-        String description = sc.nextLine();
+        boolean repeat = true;
+        while (repeat) {
+            System.out.print("Enter brand name: ");
+            String name = sc.nextLine();
+            System.out.print("Enter brand website: ");
+            String website = sc.nextLine();
+            System.out.print("Enter brand description: ");
+            String description = sc.nextLine();
 
-        if (brandRepo.isNameExists(name)){
-            System.out.println("!Brand name already exists");
-        }else {
-            brandRepo.save(new Brand(name,website,description));
+            if (brandRepo.isNameExists(name)) {
+                System.out.println("!Brand name already exists");
+            } else {
+                repeat=false;
+                brandRepo.save(new Brand(name, website, description));
+            }
         }
     }
+
     public void editName() throws SQLException {
         System.out.print("Enter brand id: ");
-        int id= sc.nextInt();
+        int id = sc.nextInt();
         sc.nextLine();
         System.out.print("Enter new name: ");
-        String name= sc.nextLine();
-        brandRepo.editName(id,name);
+        String name = sc.nextLine();
+        if (brandRepo.isNameExists(name)) {
+            System.out.println("!Name is already taken");
+        } else brandRepo.editName(id, name);
     }
+
     public void editWebsite() throws SQLException {
         System.out.print("Enter brand id: ");
-        int id= sc.nextInt();
+        int id = sc.nextInt();
         sc.nextLine();
         System.out.print("Enter new website address: ");
-        String website= sc.nextLine();
-        brandRepo.editWebsite(id,website);
+        String website = sc.nextLine();
+        brandRepo.editWebsite(id, website);
     }
+
     public void editDescription() throws SQLException {
         System.out.print("Enter brand id: ");
-        int id= sc.nextInt();
+        int id = sc.nextInt();
         sc.nextLine();
         System.out.print("Enter new description: ");
-        String description= sc.nextLine();
-        brandRepo.editDescription(id,description);
+        String description = sc.nextLine();
+        brandRepo.editDescription(id, description);
     }
+
     public void delete() throws SQLException {
         System.out.print("Enter brand id: ");
-        int id= sc.nextInt();
+        int id = sc.nextInt();
         brandRepo.delete(id);
     }
+
     public void show() throws SQLException {
         System.out.println("\t\t**BRANDS**");
         brandRepo.showBrands();

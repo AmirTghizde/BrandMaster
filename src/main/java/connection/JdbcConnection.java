278719b6508f1a40ee409a094d1a3114ac1a348f
AmirTghizde <a.taghizadehmail@gmail.com>
2024-03-29 13:40:35 +0330

    Add dotenv Dependency

--- src/main/java/connection/JdbcConnection.java
+++ src/main/java/connection/JdbcConnection.java
@@ -1,15 +1,21 @@
 package connection;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+        import io.github.cdimascio.dotenv.Dotenv;
+
+        import java.sql.Connection;
+        import java.sql.DriverManager;
+        import java.sql.SQLException;
 
 public class JdbcConnection {
     private static final Connection connection;
 
     static {
         try {
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres","amir9650");
+            Dotenv dotenv = Dotenv.configure().load();
+            String url = dotenv.get("DB_URL");
+            String username = dotenv.get("DB_USERNAME");
+            String password = dotenv.get("DB_PASSWORD");
+            connection = DriverManager.getConnection(url,username,password);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
